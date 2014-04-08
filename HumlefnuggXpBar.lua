862e8af99f63eddad24d262fde95f7a09f75a007
Jon Packer <contact@jonpacker.com>
2014-04-08 21:21:58 +0200

    save position

--- HumlefnuggXpBar.lua
+++ HumlefnuggXpBar.lua
@@ -1,7 +1,14 @@
 HFXB = {}
+HFXB.defaults = {}
 
 function HFXB.init(eventCode, addOnName)
   if addOnName ~= "HumlefnuggXpBar" then return end
+
+  HFXB.vars = ZO_SavedVars:New("HFXBSettings", 2, nil, HFXB.defaults)
+
+  if HFXB.vars.offsetX ~= nil and HFXB.vars.offsetY ~= nil then
+    HFXBFrame:SetAnchor(TOPLEFT, GuiRoot, TOPLEFT, HFXB.vars.offsetX, HFXB.vars.offsetY)
+  end
 
   HFXB.gain()
 
@@ -9,6 +16,11 @@ function HFXB.init(eventCode, addOnName)
     local bubble = CreateControlFromVirtual("HFXBBubble", HFXBFrame, "HFXBBubble", i)
     bubble:SetSimpleAnchorParent((i - 1) * 75, 0)
   end
+end
+
+function HFXB.saveFramePosition()
+  HFXB.vars.offsetX = HFXBFrame:GetLeft()
+  HFXB.vars.offsetY = HFXBFrame:GetTop()
 end
 
 function HFXB.update()
